fa7c06dfa360b79f9983e66d73b3d0f39edaea6a
izan.arnaiz.7e5 <izan.arnaiz.7e5@itb.cat>
2023-11-13 13:00:20 +0100

    1

--- ContraseñaCorrecta.py
+++ ContraseñaCorrecta.py
@@ -5,11 +5,8 @@ while clave != PWD and intento < 2 and clave != "1234":
     intento = intento + 1
     print("Clave incorrecta")
     clave = input("Dime la clave")
-    while intento == 2:
+    if intento == 2 and clave != PWD and clave != "1234":
         print("Límite de fallos superado")
-        intento = 1
-
-while intento < 2:
+if clave == PWD or clave == "1234":
     print("Bienvenido")
-    intento = 3
 print("Programa terminado")
