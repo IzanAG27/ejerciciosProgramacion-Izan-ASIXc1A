4ad38876925d5dc48d075edbbd8a2d118e3c256f
izan.arnaiz.7e5 <izan.arnaiz.7e5@itb.cat>
2023-11-21 10:42:10 +0100

    1

--- ContraseñaCorrecta.py
+++ ContraseñaCorrecta.py
@@ -1,12 +1,1 @@
-PWD = "asdasd"
-clave = input("Dime la clave: ")
-intento = 0
-while clave != PWD and intento < 2 and clave != "1234":
-    intento = intento + 1
-    print("Clave incorrecta")
-    clave = input("Dime la clave")
-    if intento == 2 and clave != PWD and clave != "1234":
-        print("Límite de fallos superado")
-if clave == PWD or clave == "1234":
-    print("Bienvenido")
-print("Programa terminado")
+print(abs(int(input("Num: "))))
